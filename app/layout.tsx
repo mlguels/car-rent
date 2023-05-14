acf0acfbc5f488e1a512c156455404e25c9845df
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-05-13 17:29:56 -0700

    Authentication finalized -NextAuth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
-import RegisterModal from './components/modals/RegisterModal'
-import NavBar from './components/navbar/NavBar'
 import './globals.css'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import ToasterProvider from './providers/ToasterProvider'
+
+import RegisterModal from './components/modals/RegisterModal'
+import LoginModal from './components/modals/LoginModal'
+import getCurrentUser from './actions/getCurrentUser'
+
+import NavBar from './components/navbar/NavBar'
  
 const font = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
@@ -11,17 +15,21 @@ export const metadata = {
   description: 'Rental Cars Inc',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
+
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
         <RegisterModal />
-        <NavBar />
+        <LoginModal />
+        <NavBar currentUser={currentUser} />
         {children}
       </body>
     </html>
